c704c9564f69454694f2b692f33710e875fd5d3f
Cristiano Ferreira <96497604+crisRPG@users.noreply.github.com>
2022-05-16 01:53:05 -0300

    Update hidra.js

--- hidra.js
+++ hidra.js
@@ -3,7 +3,9 @@ var dis;
 
 function botao() {
 
-
+//Criei o objeto "dis" e foi atribuído
+//o caminho para o "body" do HTML
+//Depois a ação para alterar a cor
     
  dis = document.getElementByTagName('body');
 
